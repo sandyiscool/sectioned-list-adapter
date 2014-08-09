21aaae5bdce5edb8a6531b1ca8bd2e2cc3196acf
sandeep <sandyiscool@gmail.com>
2014-08-10 00:55:56 +0530

    Added background colors for headers.

--- SectionedListAdapterExample/src/com/sandeep/sectionedlistadapter/example/adapter/MyListAdapter1.java
+++ SectionedListAdapterExample/src/com/sandeep/sectionedlistadapter/example/adapter/MyListAdapter1.java
@@ -1,6 +1,7 @@
 package com.sandeep.sectionedlistadapter.example.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,37 +13,38 @@ import java.util.List;
 import com.sandeep.sectionedlistadapter.SectionedListAdapter;
 import com.sandeep.sectionedlistadapter.example.R;
 
+public class MyListAdapter1 extends SectionedListAdapter<String, String> {
+	private LayoutInflater mInflater;
+	
+	int[] headerColors={Color.BLUE,Color.GREEN,Color.MAGENTA};
 
-public class MyListAdapter1 extends SectionedListAdapter<String,String>
-{
-    private LayoutInflater mInflater;
+	public MyListAdapter1(Context context,
+			LinkedHashMap<String, ? extends List<String>> data) {
+		super(data);
+		mInflater = LayoutInflater.from(context);
+	}
 
-    public MyListAdapter1(Context context, LinkedHashMap<String, ? extends List<String>> data){
-        super(data);
-        mInflater= LayoutInflater.from(context);
-    }
+	@Override
+	protected View getHeaderView(int position, View convertView,
+			ViewGroup parent) {
+		String header = (String) getItem(position);
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.view_header, null);
+		}
+		TextView txtView=(TextView) convertView.findViewById(R.id.text_main);
+		txtView.setText(header.toString());
+		txtView.setBackgroundColor(headerColors[position%headerColors.length]);
+		return convertView;
+	}
 
-    @Override
-    protected View getHeaderView(int position, View convertView, ViewGroup parent)
-    {
-        String header = (String) getItem(position);
-        if (convertView == null)
-        {
-            convertView = mInflater.inflate(R.layout.view_header, null);
-        }
-        ((TextView) convertView.findViewById(R.id.text_main)).setText(header.toString());
-        return convertView;
-    }
-
-    @Override
-    protected View getChildView(int position, View convertView, ViewGroup parent)
-    {
-        String child = (String) getItem(position);
-        if (convertView == null)
-        {
-            convertView = mInflater.inflate(R.layout.view_list_item1, null);
-        }
-        ((TextView) convertView.findViewById(R.id.text_main)).setText(child.toString());
-        return convertView;
-    }
+	@Override
+	protected View getChildView(int position, View convertView, ViewGroup parent) {
+		String child = (String) getItem(position);
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.view_list_item1, null);
+		}
+		((TextView) convertView.findViewById(R.id.text_main)).setText(child
+				.toString());
+		return convertView;
+	}
 }
